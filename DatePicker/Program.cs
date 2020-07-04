6d65bdba0a6fba61fa53598f8d396d895b5cf754
Todd Patton <64681278+Todd-ops@users.noreply.github.com>
2020-07-04 10:59:23 -0500

    Add project files.

---
+++ DatePicker/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatePicker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateList = new List<string>();
+            dateList.Add("Movie Night");
+            dateList.Add("Cook at home");
+            dateList.Add("Bowling");
+            dateList.Add("Cancun Margaritas");
+            dateList.Add("Night Walk");
+            dateList.Add("R.C.C. Dinner");
+            dateList.Add("Backyard Camp Fire");
+            dateList.Add("Card Game Night");
+            dateList.Add("Alexa Games");
+            dateList.Add("Let Becky Decide");
+
+            var randomPicker = new Random();
+            var randoString = randomPicker.Next(dateList.Count);
+            Console.WriteLine($"{dateList[randoString]}");
+
+        }
+
+        //public void RandomDatePicker()
+        
+            //var randomPicker = new Random();
+            //var randoString = randomPicker.Next(dateList.Count);
+        
+        
+    }
+}
